dbaa11f0937772fe37c1ebbc35b46c40a633cecd
AlexVR <josian.velez@upr.edu>
2018-11-14 11:20:46 -0400

    added music api

---
+++ src/Resources/MusicHandler.java
@@ -0,0 +1,31 @@
+package Resources;
+
+public class MusicHandler {
+
+
+    static String path;
+
+    static void play(){
+
+    }
+
+    static void pause(){
+
+    }
+
+    static void changeMusic(){
+
+    }
+
+    static void playEfect(){
+
+    }
+
+    static void lowerVolume(){
+
+    }
+
+    static void  higherVolume(){
+
+    }
+}
